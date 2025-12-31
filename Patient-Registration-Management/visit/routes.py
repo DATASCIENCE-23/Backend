4c0a7b90cc520b8b22e63a4f5f4ec8b170d403f1
NACHAMMAI-SN <nachammai.s.n@gmail.com>
2025-12-31 17:24:09 +0530

    Remove Visit module from Patient Registration Management

--- Patient-Registration-Management/visit/routes.py
+++
@@ -1,9 +0,0 @@
-# visit/routes.py
-from .controller import router as visit_router
-
-def register_visit_routes(app):
-    app.include_router(
-        visit_router,
-        prefix="/visits",
-        tags=["Visits"]
-    )
