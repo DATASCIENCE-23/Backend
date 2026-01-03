6f3c7818cae47c83a4e2f44751f9200f9b5af33e
S-Farhana <s.farhana1702@gmail.com>
2026-01-03 22:07:58 +0530

    Modified by Farhana: Updated User, UserRole, AuditLog models

--- Electronic-Medical-Records-Management/User_Role/User_Role_models.py
+++ Electronic-Medical-Records-Management/User_Role/User_Role_models.py
@@ -3,9 +3,21 @@ from database import Base
 from datetime import date
 
 class UserRole(Base):
-    __tablename__ = "user_roles"
+    __tablename__ = "user_role"
+    __table_args__ = {"schema": "hms"}
 
     user_role_id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey("users.user_id"), nullable=False)
-    role_id = Column(Integer, ForeignKey("roles.role_id"), nullable=False)
+
+    user_id = Column(
+        Integer,
+        ForeignKey("hms.users.user_id"),
+        nullable=False
+    )
+
+    role_id = Column(
+        Integer,
+        ForeignKey("hms.role.role_id"),
+        nullable=False
+    )
+
     assigned_date = Column(Date, default=date.today)
