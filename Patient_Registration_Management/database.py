1e108618a781012f89f11550cd306c03d7c4a665
NACHAMMAI S N <nachammai.s.n@gmail.com>
2026-01-06 21:09:30 +0530

    Update database connection string password

--- Patient_Registration_Management/database.py
+++ Patient_Registration_Management/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://hms_user:Aishu%232006@localhost:5432/hospitalmanagement"
+DATABASE_URL = "postgresql://hms_user:pwd@localhost:5432/hospitalmanagement"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
