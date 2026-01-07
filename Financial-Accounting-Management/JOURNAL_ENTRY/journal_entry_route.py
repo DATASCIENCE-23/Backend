b839d92409d0a3f5783ea0b1829d7cb5c9f5c1ca
Anandika-M <165357684+Anandika-M@users.noreply.github.com>
2026-01-07 18:39:20 +0530

    Delete Financial-Accounting-Management/JOURNAL_ENTRY directory

--- Financial-Accounting-Management/JOURNAL_ENTRY/journal_entry_route.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from JOURNAL_ENTRY.journal_entry_controller import router as journal_entry_controller
-
-router = APIRouter(
-    prefix="/journal-entries",
-    tags=["Journal Entries"]
-)
-
-router.include_router(journal_entry_controller)
