cc1093f426432c0fd25837f32d79600333be57ad
Anandika-M <165357684+Anandika-M@users.noreply.github.com>
2026-01-07 18:38:51 +0530

    Delete Financial-Accounting-Management/BUDGET directory

--- Financial-Accounting-Management/BUDGET/budget_routes.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from BUDGET.budget_controller import router as budget_controller
-
-router = APIRouter(
-    prefix="/budgets",
-    tags=["Budgets"]
-)
-
-router.include_router(budget_controller)
