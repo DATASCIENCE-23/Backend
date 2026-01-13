ca0ae18be5e9c1408c7a7d341918505a0038f668
Lokhesh.NK <108180686+LokheshNK@users.noreply.github.com>
2026-01-13 23:22:04 +0530

    fix : updated main.py

--- Electronic-Medical-Records-Management/main.py
+++ Electronic-Medical-Records-Management/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 # 🔥 CORS CONFIG (THIS FIXES AXIOS NETWORK ERROR)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app
+    allow_origins=["*"],  # React app
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
